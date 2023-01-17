9b0ef12c42fcf303ea0255b639c9312d79a8adcc
OlesiaMelnyk1 <elsonidodelmar26@gmail.com>
2023-01-17 21:23:39 +0200

    add js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,6 +1,7 @@
 const body = document.querySelector('body');
 const buttonStart = document.querySelector('[data-start]');
 const buttonStop = document.querySelector('[data-stop]');
+let intervalId = null;
 
 buttonStart.addEventListener('click', onStartColorChange);
 buttonStop.addEventListener('click', onStopColorChange);
